452f7327af5bfa2ba5c054b7cf748d6d3c0a1c44
Andrew Kachnic <ajkachnic@protonmail.com>
2021-06-17 21:15:15 -0400

    Basic repl (just echoes for now)

--- Makefile
+++ Makefile
@@ -1,6 +1,6 @@
 CC=gcc
-CFLAGS=-I. -Wall -std=c99
-DEPS = main.h
+CFLAGS=-I. -Wall -std=c99 -ledit -lm
+DEPS = main.h common.h
 OBJ = main.o
 
 %.o: %.c $(DEPS)
--- main.c
+++ main.c
@@ -1,6 +1,20 @@
 #include <stdio.h>
+#include <stdlib.h>
 
-int main(int argc, char **argv)
-{
-  puts("Hello world!");
+#include <editline/history.h>
+#include <editline/readline.h>
+
+#include "common.h"
+
+int main(int argc, char** argv) {
+  puts("Welcome to neptune!");
+
+  while (true) {
+    char* input = readline("neptune> ");
+    add_history(input);
+
+    printf("You said: %s\n", input);
+
+    free(input);
+  }
 }
